5b4a66e2f54f11b5493cc1af733c1abae6bf0489
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-04-09 08:31:44 +0200

    first commit

---
+++ src/components/Booking.tsx
@@ -0,0 +1,9 @@
+function Booking() {
+    return(
+        <div>
+            <h3>BOOKING</h3>
+        </div>
+    );
+}
+
+export default Booking;
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,9 @@
+function Contacts() {
+    return(
+        <div>
+            <h3>CONTACTS</h3>
+        </div>
+    );
+}
+
+export default Contacts;
